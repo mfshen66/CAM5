2eef355cfb1ff8c88cb7bfababf8554a4db121ee
mfshen66 <buaasmf@163.com>
2023-03-17 11:57:18 +0800

    修改测试代码

--- CAM5_mmSTL625/Test.cpp
+++ CAM5_mmSTL625/Test.cpp
@@ -11,45 +11,15 @@
 #include <iostream>
 using namespace std;
 
-
 #define FOR_NO_VERTEX_NORMAL "Debug only! For STL has no vertex normal!"
 //#define FOR_VERTEX_NORMAL    "Debug only! For STL has vertex normal!"
 
-#ifdef FOR_NO_VERTEX_NORMAL
-#undef FOR_VERTEX_NORMAL
-#endif // !FOR_NO_VERTEX_NORMAL
-
-#ifdef FOR_VERTEX_NORMAL
-#undef FOR_NO_VERTEX_NORMAL
-#endif // FOR_VERTEX_NORMAL
-
-#define POLYLINE_DISPLAY(L)									 \
-if(L) {														\
-	L->PolylineCheck();										\
-	int n_##L = 0;											\
-	for (int i = 1; i <= (L->DNum); i++)					\
-		n_##L += (L->ENum[i] - L->SNum[i] + 1);				\
-	for (size_t i = L->SNum[1]; i < n_##L; i++)				\
-	{														\
-		PNT3D p, q;											\
-		memcpy(p, &L->PTrail[i], sizeof(PNT3D));			\
-		memcpy(q, &L->PTrail[i + 1], sizeof(PNT3D));	\
-		AddLin(p, q);										\
-	}														\
-	Redraw();												\
-}
-
-#define POLYLINE_DESTROY(L) \
-if(L) {						\
-	L->Destroy();			\
-	free(L);				\
-	L=nullptr;				\
-}
+#include "MacForTest.h"
 
 // 测地等距功能测试代码
 #include <iostream>
 using namespace std;
-int calltimes = 1;
+int calltimes = 3;
 int TNum = 1;
 void CCAMDoc::OnTest()
 {
@@ -75,7 +45,7 @@ void CCAMDoc::OnTest()
 	polist1 = pGM->CreateMTIPathFromPL(polyline);
 #endif // FOR_VERTEX_NORMAL
 
-		POList polist2 = polist1->GeodesicOffsetFlexibleNew(calltimes++ * 10, pGM);
+		POList polist2 = polist1->GeodesicOffsetFlexibleNew(35, pGM);
 		POLYLINE_DISPLAY(polist1);
 #ifdef FOR_VERTEX_NORMAL
 		POLYLINE_DESTROY(polist1);
